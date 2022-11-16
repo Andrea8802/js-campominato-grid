d698cb79371130e57649335f549dd962df8458ae
Andrea8802 <andreabonaventura802@gmail.com>
2022-11-16 16:05:24 +0100

    grid js done

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// l'utente cliccando un bottone genererà una griglia 10x10 con dei numeri da 1 a 100, l'utente cliccando su di esse le colorerà di azzurro
+
+// Manipolazione DOM
+const container = document.getElementById("container");
+const genera = document.getElementById("genera");
+
+
+
+genera.addEventListener("click",
+    function(){
+        for(i = 1; i <= 100; i++){
+            const box = document.createElement("div");
+            box.classList.add("box")
+            container.append(box);
+            box.innerHTML = i;
+        }
+    }
+
+)
